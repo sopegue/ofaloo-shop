58c2021ac8f7d36dcae5198b564cf555c1f08c75
sopegue <yayasopeguesoro@gmail.com>
2020-12-07 04:44:51 +0100

    wip

---
+++ database/migrations/2020_12_07_033748_create_approves_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApprovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('approves', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user');
+            $table->bigInteger('evaluation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('approves');
+    }
+}
